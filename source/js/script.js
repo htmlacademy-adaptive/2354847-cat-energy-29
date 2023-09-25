83902e3e5e5e0cff99a80f2328962641eacb640c
Xenia-Ale <xenia.aleshina01@gmail.com>
2023-09-25 10:06:51 +0300

    правка

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 let nav = document.querySelector('.main-nav');
-
 let navButton = document.querySelector('.main-nav__toggle');
+
+nav.classList.remove('main-nav--nojs');
+
 navButton.onclick = function() {
 
   nav.classList.toggle('main-nav--closed');
